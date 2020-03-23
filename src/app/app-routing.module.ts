8fb90fac4be9f1533c9af3da1839ae9c34491700
Xamp <deivid.develop@gmail.com>
2020-03-23 19:46:25 +0100

    Remodeling of the info-model-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes }  from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { Error404Component } from './components/error404/error404.component';
 import { FormLoginComponent } from './components/form-login/form-login.component';
-import { Model3dComponent } from './components/model3d/model3d.component';
 import { InfoModelComponent } from './components/info-model/info-model.component';
+import { FormRegisterComponent } from './components/form-register/form-register.component';
 
 const appRoutes: Routes = [
   {path: 'login', component: FormLoginComponent},
+  {path: 'register', component: FormRegisterComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'model/:modelId', component: Model3dComponent},
-  {path: 'model/info', component: InfoModelComponent},
+  {path: 'model/:modelId', component: InfoModelComponent},
   {path: '404', component: Error404Component},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', redirectTo: '404'}
