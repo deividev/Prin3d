a82a2a8c34e3dfa1f0a871000f95da91c210dacc
Xamp <deivid.develop@gmail.com>
2020-03-26 23:04:33 +0100

    Added cosmetic changes to the forms

--- src/app/models/model3d.ts
+++ src/app/models/model3d.ts
@@ -1,12 +1,12 @@
 export interface Model3d {
   id?: string;
   title?: string;
-  userId?: string;
+  author_id?: string;
   img?: string;
   model?: string;
   likes?: number;
   downloads?: number;
-  categoryId?: string;
+  category_id?: string;
   description?: string;
   settings?: string;
   custom?: string;
