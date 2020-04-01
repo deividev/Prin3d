82aace9b2a354247e0668c3d318dd534ab5cdca2
Xamp <deivid.develop@gmail.com>
2020-04-01 21:54:30 +0200

    Working on the modal collection

--- src/app/components/modal-collection/modal-collection.component.ts
+++ src/app/components/modal-collection/modal-collection.component.ts
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalCollectionComponent implements OnInit {
 
+  modalIsVisible = false;
+
   constructor() { }
 
   ngOnInit(): void {
 
   }
+
 
 }
