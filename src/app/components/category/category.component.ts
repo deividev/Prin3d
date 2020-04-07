b481c13d34bc54d9aea5bbfae8335731b6ac91fc
Xamp <deivid.develop@gmail.com>
2020-04-07 17:57:50 +0200

    Added category views

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Model3dService } from 'src/app/services/model3d.service';
+import { Model3d } from 'src/app/models/model3d';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+  public featuredModels: Array<Model3d> = [];
 
-  constructor() { }
+
+  public featuredTitle: string = "featured";
+
+  constructor(private model3dService: Model3dService,) { }
 
   ngOnInit(): void {
+    this.model3dService.getModels().subscribe((result) => {
+      //Ordena el resultado de los mas votados de mayor a menor y coge los 8 primeros;
+      this.featuredModels = result;
+    });
   }
-
 }
