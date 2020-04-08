7be133b9100257bf584345f30f55630e1cc2f462
Xamp <deivid.develop@gmail.com>
2020-04-08 15:24:52 +0200

    testing with the DB to paint the categories with their corresponding models

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Model3dService } from 'src/app/services/model3d.service';
 import { Model3d } from 'src/app/models/model3d';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-category',
@@ -8,17 +9,32 @@ import { Model3d } from 'src/app/models/model3d';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
-  public featuredModels: Array<Model3d> = [];
 
+  @Input() categories: any;
+  @Input() listModels: any;
+  @Input() title: string;
 
   public featuredTitle: string = "featured";
+  public featuredModels: Array<Model3d> = [];
 
-  constructor(private model3dService: Model3dService,) { }
+  constructor(private model3dService: Model3dService,
+              private categoriesService: CategoriesService)
+              {
+
+
+          }
 
   ngOnInit(): void {
     this.model3dService.getModels().subscribe((result) => {
       //Ordena el resultado de los mas votados de mayor a menor y coge los 8 primeros;
       this.featuredModels = result;
+    });
+
+    this.categoriesService.getCategories().subscribe((result) => {
+      //Me traigo todas las categorias de la DB
+      this.categories = result;
+    }, error => {
+      console.error(error)
     });
   }
 }
