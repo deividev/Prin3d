376670f5d59c18c24d474bca67898748703fa048
david <deivid.develop@gmail>
2020-06-07 16:41:33 +0200

    Update images

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: 'https://5e6602fb2aea440016afb739.mockapi.io',
-  apiBack: 'http://localhost:3000/api',
+  // 'http://localhost:3000/api'
+  apiBack: 'https://factory3d-api.herokuapp.com/api'
 };
 
 /*
