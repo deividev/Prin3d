fae69b60787fdaed4dc3e0a20bca39147692738d
david <deivid.develop@gmail>
2020-06-07 17:24:20 +0200

    Update server

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-const express = require('express'); const app = express(); app.use(express.static('./dist/print3d')); app.get('/*', (req, res) => res.sendFile('index.html', {root: 'dist/print3d/'}), ); app.listen(process.env.PORT || 8080);
+const express = require('express');
+const app = express();
+app.use(express.static('./dist/print3d'));
+app.get('/*', (req, res) => res.sendFile('index.html', {root: 'dist/print3d/'}), );
+app.listen(process.env.PORT || 8080);
